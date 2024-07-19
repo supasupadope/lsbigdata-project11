ca0082653cb708fb23e33273d704263f5093de5a
Issac Lee <issactoast@gmail.com>
2024-07-19 12:03:16 +0900

    stat simulator

--- code/stat1.py
+++ code/stat1.py
@@ -11,11 +11,19 @@ X(1)
 # 베르누이 확률변수 모수: p 만들어보세요!
 # num = 3
 # p = 0.5
-def Y(num, p):
+def Y(num):
     x=np.random.rand(num)
     return np.where(x < p, 1, 0)
 
+Y(num=30)
+sum(np.array([1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1,
+       0, 1, 1, 1, 1, 1, 0, 1]))
+       
+       
+2/3       
+       
 # sum(Y(num=100, p=0.5))/100
+Y(num=10)
 Y(num=1, p=0.5)
 Y(num=100, p=0.5).mean()
 Y(num=100000, p=0.5).mean()
@@ -30,12 +38,14 @@ def Z():
 Z()
 
 # p = np.array([0.2, 0.5, 0.3)
-def Z(p):
+def Z():
     x=np.random.rand(1)
     p_cumsum=p.cumsum()
     return np.where(x < p_cumsum[0], 0, np.where(x < p_cumsum[1], 1, 2))
 
-p = np.array([0.1, 0.6, 0.3])
-Z(p)
+p = np.array([0.2, 0.3, 0.5])
+Z()
 
 
+# E[X]
+sum(np.arange(4) * np.array([1, 2, 2, 1])/6)
---
+++ code/stat2.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 예제 넘파이 배열 생성
+data = np.random.rand(10)
+# sum(data < 0.18)
+
+# 히스토그램 그리기
+plt.clf()
+plt.hist(data, bins=4, alpha=0.7, color='blue')
+plt.title('Histogram of Numpy Vector')
+plt.xlabel('Value')
+plt.ylabel('Frequency')
+plt.grid(False)
+plt.show()
+
+
+# x=np.random.rand(50000) \
+#     .reshape(-1, 5) \
+#     .mean(axis=1)
+x=np.random.rand(10000, 5).mean(axis=1)
+plt.hist(x, bins=30, alpha=0.7, color='blue')
+plt.title('Histogram of Numpy Vector')
+plt.xlabel('Value')
+plt.ylabel('Frequency')
+plt.grid(False)
+plt.show()
