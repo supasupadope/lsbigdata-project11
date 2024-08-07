475a64425600f138ee97ec96acf0dcacb55fceb6
Issac Lee <issactoast@gmail.com>
2024-08-07 09:59:07 +0900

    geojson

--- code/textbook-chap11.py
+++ code/textbook-chap11.py
@@ -1,6 +1,29 @@
 import json
 
-geo = json.load(open("./data/bigfile/SIG.geojson", encoding="UTF-8"))
+geo_seoul = json.load(open("./data/SIG_Seoul.geojson", encoding="UTF-8"))
 
-geo
+
+type(geo_seoul)
+len(geo_seoul)
+geo_seoul.keys()
+geo_seoul["features"][0]
+len(geo_seoul["features"])
+len(geo_seoul["features"][0])
+geo_seoul["features"][0].keys()
+geo_seoul["features"][0]["properties"]
+geo_seoul["features"][0]["geometry"]
+
+
+coordinate_list=geo_seoul["features"][0]["geometry"]["coordinates"]
+len(coordinate_list[0][0])
+coordinate_list[0][0]
+
+
+import numpy as np
+coordinate_array=np.array(coordinate_list[0][0])
+x=coordinate_array[:,0]
+y=coordinate_array[:,1]
+
+
+
 
