0529abf23be64fe1e360698fd4320e70f8beb91d
Issac Lee <issactoast@gmail.com>
2024-08-08 17:29:17 +0900

    ㅇㅇ

--- code/houseprice7.py
+++ code/houseprice7.py
@@ -1,3 +1,7 @@
+# 원하는 변수를 사용해서 회귀모델을 만들고, 제출할것!
+# 원하는 변수 2개
+# 회귀모델을 통한 집값 예측
+
 # 필요한 패키지 불러오기
 import numpy as np
 import pandas as pd
@@ -10,28 +14,20 @@ house_test=pd.read_csv("./data/houseprice/test.csv")
 sub_df=pd.read_csv("./data/houseprice/sample_submission.csv")
 
 ## 이상치 탐색
-# house_train=house_train.query("GrLivArea <= 4500")
-
+house_train=house_train.query("GrLivArea <= 4500")
 
 ## 회귀분석 적합(fit)하기
 # house_train["GrLivArea"]   # 판다스 시리즈
 # house_train[["GrLivArea"]] # 판다스 프레임
-# 숫자형 변수만 선택하기
-x = house_train.select_dtypes(include=[int, float])
-# 필요없는 칼럼 제거하기
-x = x.iloc[:,1:-1]
+house_train["Neighborhood"]
+neighborhood_dummies = pd.get_dummies(
+    house_train["Neighborhood"],
+    drop_first=True
+    )
+# pd.concat([df_a, df_b], axis=1)
+x= pd.concat([house_train[["GrLivArea", "GarageArea"]], 
+             neighborhood_dummies], axis=1)
 y = house_train["SalePrice"]
-
-
-# 변수별로 결측값 채우기
-fill_values = {
-    'LotFrontage': x["LotFrontage"].mean(),
-    'MasVnrArea': x["MasVnrArea"].mean(),
-    'GarageYrBlt': x["GarageYrBlt"].mean()
-}
-x = x.fillna(value=fill_values)
-x.isna().sum()
-x.mean()
 
 # 선형 회귀 모델 생성
 model = LinearRegression()
@@ -43,18 +39,18 @@ model.fit(x, y)  # 자동으로 기울기, 절편 값을 구해줌
 model.coef_      # 기울기 a
 model.intercept_ # 절편 b
 
-# 테스트 데이터 예측
-test_x = house_test.select_dtypes(include=[int, float])
-test_x = test_x.iloc[:,1:]
-
-# 결측치 채우기
-test_x=test_x.fillna(test_x.mean())
-
+neighborhood_dummies_test = pd.get_dummies(
+    house_test["Neighborhood"],
+    drop_first=True
+    )
+test_x = house_test[["GrLivArea", "GarageArea"]]
+test_x
 
 # 결측치 확인
-test_x.isna().sum()
-test_x.mean()
-# 테스트 데이터 집값 예측
+test_x["GrLivArea"].isna().sum()
+test_x["GarageArea"].isna().sum()
+test_x=test_x.fillna(house_test["GarageArea"].mean())
+
 pred_y=model.predict(test_x) # test 셋에 대한 집값
 pred_y
 
@@ -63,4 +59,19 @@ sub_df["SalePrice"] = pred_y
 sub_df
 
 # csv 파일로 내보내기
-sub_df.to_csv("./data/houseprice/sample_submission9.csv", index=False)
+sub_df.to_csv("./data/houseprice/sample_submission7.csv", index=False)
+
+# 시각화
+# 직선값 계산
+y_pred = model.predict(x)
+
+# 데이터와 회귀 직선 시각화
+plt.scatter(x, y, color='blue', label='data')
+plt.plot(x, y_pred, color='red', label='regression')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.xlim([0, 5000])
+plt.ylim([0, 900000])
+plt.legend()
+plt.show()
+plt.clf()
