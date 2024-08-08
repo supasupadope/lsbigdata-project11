708d1819b5cd14511d845bcbdf9bf91d61ee187a
Issac Lee <issactoast@gmail.com>
2024-08-08 14:17:12 +0900

    dd

---
+++ code/plotly-express.py
@@ -0,0 +1,38 @@
+# 데이터 패키지 설치
+# !pip install palmerpenguins
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from palmerpenguins import load_penguins
+
+penguins = load_penguins()
+penguins.head()
+
+# x: bill_length_mm
+# y: bill_depth_mm  
+fig = px.scatter(
+    penguins,
+    x="bill_length_mm",
+    y="bill_depth_mm",
+    color="species"
+)
+# 레이아웃 업데이트
+fig.update_layout(
+    title=dict(text="팔머펭귄 종별 부리 길이 vs. 깊이", font=dict(color="white")),
+    paper_bgcolor="black",
+    plot_bgcolor="black",
+    font=dict(color="white"),
+    xaxis=dict(
+        title=dict(text="부리 길이 (mm)", font=dict(color="white")), 
+        tickfont=dict(color="white"),
+        gridcolor='rgba(255, 255, 255, 0.2)'  # 그리드 색깔 조정
+    ),
+    yaxis=dict(
+        title=dict(text="부리 깊이 (mm)", font=dict(color="white")), 
+        tickfont=dict(color="white"),
+        gridcolor='rgba(255, 255, 255, 0.2)'  # 그리드 색깔 조정
+    ),
+    legend=dict(font=dict(color="white")),
+)
+
+fig.show()
