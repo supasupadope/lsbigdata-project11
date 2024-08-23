acfe31ef43932c923dfbeafc6986d9a19a552983
Issac Lee <issactoast@gmail.com>
2024-08-23 09:13:32 +0900

    테스트

--- code/houseprice8.py
+++ code/houseprice8.py
@@ -14,41 +14,47 @@ house_train=pd.read_csv("./data/houseprice/train.csv")
 house_test=pd.read_csv("./data/houseprice/test.csv")
 sub_df=pd.read_csv("./data/houseprice/sample_submission.csv")
 
-## 이상치 탐색
-house_train=house_train.query("GrLivArea <= 4500")
+## 이상치 탐색 (여기 넣으면 안됨!)
+# house_train=house_train.query("GrLivArea <= 4500")
 
 house_train.shape
 house_test.shape
-train_n=house_train.shape[0]
+train_n=len(house_train)
 
-df=pd.concat([house_train, house_test],
-             ignore_index=True)
-df
-
-neighborhood_dummies = pd.get_dummies(
-    df["Neighborhood"],
+# 통합 df 만들기 + 더미코딩
+df = pd.concat([house_train, house_test], ignore_index=True)
+df = pd.get_dummies(
+    df,
+    columns= ["Neighborhood"],
     drop_first=True
     )
-neighborhood_dummies
+df
 
-x=pd.concat([df[["GrLivArea", "GarageArea"]], 
-            neighborhood_dummies], axis=1)
-
-y=df["SalePrice"]
-
-train_x=x.iloc[:train_n,]
-test_x=x.iloc[train_n:,]
-train_y=y[:train_n]
+# train / test 데이터셋
+train_df=df.iloc[:train_n,]
+test_df=df.iloc[train_n:,]
 
 # Validation 셋(모의고사 셋) 만들기
 np.random.seed(42)
 val_index=np.random.choice(np.arange(train_n), size=438, replace=False)
 val_index
 
-valid_x=train_x.loc[val_index]  # 30%
-train_x=train_x.drop(val_index) # 70%
-valid_y=train_y[val_index]      # 30%
-train_y=train_y.drop(val_index) # 70%
+# train => valid / train 데이터셋
+valid_df=train_df.loc[val_index]  # 30%
+train_df=train_df.drop(val_index) # 70%
+
+## 이상치 탐색
+train_df=train_df.query("GrLivArea <= 4500")
+
+# x, y 나누기
+# regex (Regular Expression, 정규방정식)
+# ^ 시작을 의미, $ 끝남을 의미, | or를 의미
+selected_columns=train_df.filter(regex='^GrLivArea$|^GarageArea$|^Neighborhood_').columns
+train_x=train_df[selected_columns]
+train_y=train_df["SalePrice"]
+
+valid_x=valid_df[selected_columns]
+valid_y=valid_df["SalePrice"]
 
 # 선형 회귀 모델 생성
 model = LinearRegression()
@@ -59,10 +65,6 @@ model.fit(train_x, train_y)  # 자동으로 기울기, 절편 값을 구해줌
 # 성능 측정 ()
 y_hat=model.predict(valid_x)
 np.sqrt(np.mean((valid_y-y_hat)**2))
-
-38986
-
-
 
 pred_y=model.predict(test_x) # test 셋에 대한 집값
 pred_y
