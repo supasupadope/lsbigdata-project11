7244ba1bef1027a511225c07bfd270fc96b75ea9
Issac Lee <issactoast@gmail.com>
2024-08-26 16:17:17 +0900

    mat2-lasso

---
+++ code/mat2-lasso.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+# 회귀분석 데이터행렬
+x=np.array([13, 15,
+           12, 14,
+           10, 11,
+           5, 6]).reshape(4, 2)
+x
+vec1=np.repeat(1, 4).reshape(4, 1)
+matX=np.hstack((vec1, x))
+y=np.array([20, 19, 20, 12]).reshape(4, 1)
+matX
+
+# minimize로 라쏘 베타 구하기
+from scipy.optimize import minimize
+
+def line_perform_lasso(beta):
+    beta=np.array(beta).reshape(3, 1)
+    a=(y - matX @ beta)
+    return (a.transpose() @ a) + 3*np.abs(beta).sum()
+
+line_perform([8.55,  5.96, -4.38])
+line_perform([3.76,  1.36, 0])
+line_perform_lasso([8.55,  5.96, -4.38])
+line_perform_lasso([3.76,  1.36, 0])
+
+# 초기 추정값
+initial_guess = [0, 0, 0]
+
+# 최소값 찾기
+result = minimize(line_perform_lasso, initial_guess)
+
+# 결과 출력
+print("최소값:", result.fun)
+print("최소값을 갖는 x 값:", result.x)
+
