f204fefb5b00aaeb3a620e741cc74c6112f5d493
Issac Lee <issactoast@gmail.com>
2024-08-26 17:15:01 +0900

    lass 회귀 베타

--- code/mat2-lasso.py
+++ code/mat2-lasso.py
@@ -17,10 +17,10 @@ from scipy.optimize import minimize
 def line_perform_lasso(beta):
     beta=np.array(beta).reshape(3, 1)
     a=(y - matX @ beta)
-    return (a.transpose() @ a) + 3*np.abs(beta).sum()
+    return (a.transpose() @ a) + 500*np.abs(beta[1:]).sum()
 
 line_perform_lasso([8.55,  5.96, -4.38])
-line_perform_lasso([3.76,  1.36, 0])
+line_perform_lasso([8.14,  0.96, 0])
 
 # 초기 추정값
 initial_guess = [0, 0, 0]
@@ -31,4 +31,37 @@ result = minimize(line_perform_lasso, initial_guess)
 # 결과 출력
 print("최소값:", result.fun)
 print("최소값을 갖는 x 값:", result.x)
+
+[8.55,  5.96, -4.38] # 람다 0
+
+[8.14,  0.96, 0] # 람다 3
+# 예측식: y_hat = 8.14 + 0.96 * X1 + 0 * X2
+
+[17.74, 0, 0] # 람다 500
+# 예측식: y_hat = 17.74 + 0 * X1 + 0 * X2
+
+# 람다 값에 따라 변수 선택 된다.
+# X 변수가 추가되면, trainX에서는 성능 항상 좋아짐.
+# X 변수가 추가되면, validX에서는 좋아졌다가 나빠짐(오버피팅)
+# 어느 순간 X 변수 추가하는 것을 멈춰야 함.
+# 람다 0부터 시작: 내가 가진 모든 변수를 넣겠다!
+# 점점 람다를 증가: 변수가 하나씩 빠지는 효과
+# validX에서 가장 성능이 좋은 람다를 선택!
+# 변수가 선택됨을 의미.
+
+# (X^T X)^-1
+# X의 칼럼에 선형 종속인 애들 있다: 다중공선성이 존재한다.
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ code/mat3.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+from scipy.stats import uniform
+from sklearn.linear_model import LinearRegression
+
+# 20차 모델 성능을 알아보자능
+np.random.seed(42)
+x = uniform.rvs(size=30, loc=-4, scale=8)
+y = np.sin(x) + norm.rvs(size=30, loc=0, scale=0.3)
+
+import pandas as pd
+df = pd.DataFrame({
+    "y" : y,
+    "x" : x
+})
+df
+
+train_df = df.loc[:19]
+train_df
+
+for i in range(2, 21):
+    train_df[f"x{i}"] = train_df["x"] ** i
+    
+# 'x' 열을 포함하여 'x2'부터 'x20'까지 선택.
+train_x = train_df[["x"] + [f"x{i}" for i in range(2, 21)]]
+train_y = train_df["y"]
+
+from sklearn.linear_model import Lasso
+model= Lasso(alpha=0.1)
+model.fit(train_x, train_y)
+
+valid_df = df.loc[20:]
+valid_df
+
+for i in range(2, 21):
+    valid_df[f"x{i}"] = valid_df["x"] ** i
+
+# 'x' 열을 포함하여 'x2'부터 'x20'까지 선택.
+valid_x = valid_df[["x"] + [f"x{i}" for i in range(2, 21)]]
+
+
+# 모델 성능
+y_hat_train = model.predict(train_x)
+y_hat_val = model.predict(valid_x)
+
+sum((train_df["y"] - y_hat_train)**2)
+sum((valid_df["y"] - y_hat_val)**2)
