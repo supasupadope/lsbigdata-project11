fc0bb01cdf8df2fb8bca74d227f3eeb8fe854680
Issac Lee <issactoast@gmail.com>
2024-09-06 14:30:52 +0900

    confusion matrix

--- code/dct4-entropy.py
+++ code/dct4-entropy.py
@@ -17,44 +17,17 @@ h_1_r
 # 엘라스틱 넷 & 디시젼트리 회귀모델 사용
 # 모든 변수 자유롭게 사용!
 # 종속변수: bill_length_mm
-
 import pandas as pd
 import numpy as np
 from palmerpenguins import load_penguins
 from sklearn.preprocessing import OneHotEncoder
 
 penguins = load_penguins()
-penguins.head()
+penguins=penguins.dropna()
 
-penguins=penguins.drop("species", axis=1)
+df_X=penguins.drop("species", axis=1)
+df_X=df_X[["bill_length_mm", "bill_depth_mm"]]
 y=penguins[['species']]
-
-
-## Nan 채우기
-quantitative = penguins.select_dtypes(include = [int, float])
-quantitative.isna().sum()
-quant_selected = quantitative.columns[quantitative.isna().sum() > 0]
-
-for col in quant_selected:
-    penguins[col].fillna(penguins[col].mean(), inplace=True)
-penguins[quant_selected].isna().sum()
-
-## 범주형 채우기
-qualitative = penguins.select_dtypes(include = [object])
-qualitative.isna().sum()
-qual_selected = qualitative.columns[qualitative.isna().sum() > 0]
-
-for col in qual_selected:
-    penguins[col].fillna(penguins[col].mode()[0], inplace=True)
-penguins[qual_selected].isna().sum()
-
-df = penguins
-df = pd.get_dummies(
-    df,
-    columns = df.select_dtypes(include=[object]).columns,
-    drop_first = True
-)
-df
 
 
 # 모델 생성
@@ -62,7 +35,10 @@ from sklearn.tree import DecisionTreeClassifier
 
 ## 하이퍼파라미터 튜닝
 from sklearn.model_selection import GridSearchCV
-model = DecisionTreeClassifier(random_state=42)
+model = DecisionTreeClassifier(
+    criterion='entropy',
+    random_state=42)
+
 param_grid={
     'max_depth': np.arange(7, 20, 1),
     'min_samples_split': np.arange(10, 30, 1)
@@ -75,7 +51,7 @@ grid_search=GridSearchCV(
     cv=5
 )
 
-grid_search.fit(x,y)
+grid_search.fit(df_X,y)
 
 grid_search.best_params_ #8, 22
 grid_search.cv_results_
@@ -85,7 +61,20 @@ best_model=grid_search.best_estimator_
 model = DecisionTreeClassifier(random_state=42,
                               max_depth=2,
                               min_samples_split=22)
-model.fit(x,y)
+model.fit(df_X,y)
 
 from sklearn import tree
 tree.plot_tree(model)
+
+
+from sklearn.metrics import confusion_matrix
+# 아델리: 'A'
+# 친스트랩: 'C'
+y_true = np.array(['A', 'A', 'C', 'A', 'C', 'C', 'C'])
+y_pred = np.array(['A', 'C', 'A', 'A', 'A', 'C', 'C'])
+
+confusion_matrix(y_true=y_true, y_pred=y_pred)
+
+
+
+
--- code/dct5-entropy-detail.py
+++ code/dct5-entropy-detail.py
@@ -18,13 +18,11 @@ df=df.rename(columns={
     'bill_depth_mm': 'x2'})
 df
 
-
 # x1, x2 산점도를 그리되, 점 색깔은 펭귄 종별 다르게 그리기!
 import seaborn as sns
 
 sns.scatterplot(data=df, x="x1", y="x2", hue='y')
 plt.axvline(x=42.4)
-plt.axhline(y=16.4, color="red")
 
 # Q. 나누기 전 현재의 엔트로피?
 # Q. 45로 나눴을때, 엔트로피 평균은 얼마인가요?
@@ -57,67 +55,9 @@ entropy_df = pd.DataFrame({
 
 entropy_df.iloc[np.argmin(entropy_df['entropy']),:]
 
-# 기준 42.4 , entropy 0.804269
+# standard    16.400000
+# entropy      0.834074
 
-
-# 1번 그룹은 어떤 종류로 예측하나요?
-# 2번 그룹은 어떤 종류로 예측하나요?
-y_hat1=df.query("x1 < 45")['y'].mode()
-y_hat2=df.query("x1 >= 45")['y'].mode()
-
-# 각 그룹 엔트로피는 얼마 인가요?
-p_1=df.query("x1 < 45")['y'].value_counts() / len(df.query("x1 < 45")['y'])
-entropy1=-sum(p_1 * np.log2(p_1))
-
-p_2=df.query("x1 >= 45")['y'].value_counts() / len(df.query("x1 >= 45")['y'])
-entropy2=-sum(p_2 * np.log2(p_2))
-
-entropy_x145=(n1 * entropy1 + n2 * entropy2)/(n1 + n2)
-entropy_x145
-
-
-# 기준값 x를 넣으면 entropy값이 나오는 함수는?
-# x1 기준으로 최적 기준값은 얼마인가?
-def my_entropy(x):
-    n1=df.query(f"x < {x}").shape[0]  # 1번 그룹
-    n2=df.query(f"x >= {x}").shape[0] # 2번 그룹
-    y_hat1=df.query(f"x < {x}").mean()[0]
-    y_hat2=df.query(f"x >= {x}").mean()[0]
-    mse1=np.mean((df.query(f"x < {x}")["y"] - y_hat1)**2)
-    mse2=np.mean((df.query(f"x >= {x}")["y"] - y_hat2)**2)
-    return float((mse1* n1 + mse2 * n2)/(n1+n2))
-
-my_mse(15)
-my_mse(13.71)
-my_mse(14.01)
-
-df["x"].min()
-df["x"].max()
-
-# 13~22 사이 값 중 0.01 간격으로 MSE 계산을 해서
-# minimize 사용해서 가장 작은 MSE가 나오는 x 찾아보세요!
-x_values=np.arange(13.2, 16.4, 0.01)
-nk=x_values.shape[0]
-result=np.repeat(0.0, nk)
-for i in range(nk):
-    result[i]=my_mse(x_values[i])
-
-result
-x_values[np.argmin(result)]
-# 14.01, 16.42, 19.4
-
-# x, y 산점도를 그리고, 빨간 평행선 4개 그려주세요!
-import matplotlib.pyplot as plt
-
-df.plot(kind="scatter", x="x", y="y")
-thresholds=[14.01, 16.42, 19.4]
-df["group"]=np.digitize(df["x"], thresholds)
-y_mean=df.groupby("group").mean()["y"]
-k1=np.linspace(13, 14.01, 100)
-k2=np.linspace(14.01, 16.42, 100)
-k3=np.linspace(16.42, 19.4, 100)
-k4=np.linspace(19.4, 22, 100)
-plt.plot(k1, np.repeat(y_mean[0],100), color="red")
-plt.plot(k2, np.repeat(y_mean[1],100), color="red")
-plt.plot(k3, np.repeat(y_mean[2],100), color="red")
-plt.plot(k4, np.repeat(y_mean[3],100), color="red")
+# standard    42.400000
+# entropy      0.804269
+# x1, 42.4 선택: 엔트로피가 더 낮음!
