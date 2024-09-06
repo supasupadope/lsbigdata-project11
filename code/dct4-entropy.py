7060e19a6a561e764bfea9a96a42ca955b4fd399
Issac Lee <issactoast@gmail.com>
2024-09-06 14:36:40 +0900

    혼동행렬

--- code/dct4-entropy.py
+++ code/dct4-entropy.py
@@ -69,11 +69,18 @@ tree.plot_tree(model)
 
 from sklearn.metrics import confusion_matrix
 # 아델리: 'A'
-# 친스트랩: 'C'
+# 친스트랩(아델리 아닌것): 'C'
 y_true = np.array(['A', 'A', 'C', 'A', 'C', 'C', 'C'])
 y_pred = np.array(['A', 'C', 'A', 'A', 'A', 'C', 'C'])
 
-confusion_matrix(y_true=y_true, y_pred=y_pred)
+confusion_matrix(y_true=y_true, 
+                 y_pred=y_pred,
+                 labels=["A", "C"])
+
+
+
+
+
 
 
 
