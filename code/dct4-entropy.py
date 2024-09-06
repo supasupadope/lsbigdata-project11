7046ea46603061dd7338bf639540ea2bed696b32
Issac Lee <issactoast@gmail.com>
2024-09-06 14:38:59 +0900

    confusion mat dct6

--- code/dct4-entropy.py
+++ code/dct4-entropy.py
@@ -67,18 +67,6 @@ from sklearn import tree
 tree.plot_tree(model)
 
 
-from sklearn.metrics import confusion_matrix
-# 아델리: 'A'
-# 친스트랩(아델리 아닌것): 'C'
-y_true = np.array(['A', 'A', 'C', 'A', 'C', 'C', 'C'])
-y_pred = np.array(['A', 'C', 'A', 'A', 'A', 'C', 'C'])
-
-confusion_matrix(y_true=y_true, 
-                 y_pred=y_pred,
-                 labels=["A", "C"])
-
-
-
 
 
 
