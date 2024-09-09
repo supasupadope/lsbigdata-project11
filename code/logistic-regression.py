2181367c5a07926b5a76db0d32084b7de49f4225
Issac Lee <issactoast@gmail.com>
2024-09-09 11:22:12 +0900

    로지스틱

---
+++ code/logistic-regression.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+
+# 워킹 디렉토리 설정
+import os
+cwd=os.getcwd()
+parent_dir = os.path.dirname(cwd)
+os.chdir(parent_dir)
+
+admission_data = pd.read_csv("./data/admission.csv")
+print(admission_data.shape)
+
+# GPA: 학점
+# GRE: 대학원 입학시험 (영어, 수학)
+
+# 합격을 한 사건: Admit
+# Admit의 확률 오즈(Odds)는?
+# P(Admit)=합격 인원 / 전체 학생
+p_hat = admission_data['admit'].mean()
+p_hat / (1 - p_hat)
+
+# p(A): 0.5보다 큰 경우 -> 오즈비: 무한대에 가까워짐
+# p(A): 0.5 -> 오즈비: 1
+# p(A): 0.5보다 작은 경우 -> 오즈비: 0에 가까워짐
+# 확률의 오즈비가 갖는 값의 범위: 0~무한대
+
+admission_data['rank'].unique()
+
+grouped_data = admission_data \
+    .groupby('rank', as_index=False) \
+    .agg(p_admit=('admit', 'mean'), )
+grouped_data['odds'] = grouped_data['p_admit'] / (1 - grouped_data['p_admit'])
+print(grouped_data)
+
+
+
+# 확률이 오즈비가 3!
+# P(A)?
+
+# admission 데이터 산점도 그리기
+# x: gre, y: admit
+# admission_data
+import seaborn as sns
+
+sns.stripplot(data=admission_data,
+              x='rank', y='admit', jitter=0.3, alpha=0.3)
+
+sns.scatterplot(data=grouped_data,
+              x='rank', y='p_admit')
+
+sns.regplot(data=grouped_data, x='rank', y='p_admit')
+
+
+odds_data = admission_data.groupby('rank').agg(p_admit=('admit', 'mean')).reset_index()
+odds_data['odds'] = odds_data['p_admit'] / (1 - odds_data['p_admit'])
+odds_data['log_odds'] = np.log(odds_data['odds'])
+print(odds_data)
+
+
+sns.regplot(data=odds_data, x='rank', y='log_odds')
+
+
+
+
+
