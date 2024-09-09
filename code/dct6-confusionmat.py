4ec6cd154d48b98d9acbed2c98f2135b6e43fd60
Issac Lee <issactoast@gmail.com>
2024-09-09 09:32:16 +0900

    admission csv

--- code/dct6-confusionmat.py
+++ code/dct6-confusionmat.py
@@ -4,7 +4,8 @@ import numpy as np
 # 아델리: 'A'
 # 친스트랩(아델리 아닌것): 'C'
 y_true = np.array(['A', 'A', 'C', 'A', 'C', 'C', 'C'])
-y_pred = np.array(['A', 'C', 'A', 'A', 'A', 'C', 'C'])
+y_pred1 = np.array(['A', 'C', 'A', 'A', 'A', 'C', 'C'])
+y_pred2 = np.array(['C', 'A', 'A', 'A', 'C', 'C', 'C'])
 
 conf_mat=confusion_matrix(y_true=y_true, 
                           y_pred=y_pred,
