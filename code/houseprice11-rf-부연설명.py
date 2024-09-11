4730cdedcbc78248e731790e7ed8ec1a865fbfb8
Issac Lee <issactoast@gmail.com>
2024-09-11 14:08:06 +0900

    btstrap commit

---
+++ code/houseprice11-rf-부연설명.py
@@ -0,0 +1,113 @@
+# 필요한 패키지 불러오기
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+# 워킹 디렉토리 설정
+import os
+cwd=os.getcwd()
+parent_dir = os.path.dirname(cwd)
+os.chdir(parent_dir)
+
+## 필요한 데이터 불러오기
+house_train=pd.read_csv("./data/houseprice/train.csv")
+house_test=pd.read_csv("./data/houseprice/test.csv")
+sub_df=pd.read_csv("./data/houseprice/sample_submission.csv")
+
+## NaN 채우기
+# 각 숫치형 변수는 평균 채우기
+# 각 범주형 변수는 Unknown 채우기
+house_train.isna().sum()
+house_test.isna().sum()
+
+## 숫자형 채우기
+quantitative = house_train.select_dtypes(include = [int, float])
+quantitative.isna().sum()
+quant_selected = quantitative.columns[quantitative.isna().sum() > 0]
+
+for col in quant_selected:
+    house_train[col].fillna(house_train[col].mean(), inplace=True)
+house_train[quant_selected].isna().sum()
+
+## 범주형 채우기
+qualitative = house_train.select_dtypes(include = [object])
+qualitative.isna().sum()
+qual_selected = qualitative.columns[qualitative.isna().sum() > 0]
+
+for col in qual_selected:
+    house_train[col].fillna("unknown", inplace=True)
+house_train[qual_selected].isna().sum()
+
+
+# test 데이터 채우기
+## 숫자형 채우기
+quantitative = house_test.select_dtypes(include = [int, float])
+quantitative.isna().sum()
+quant_selected = quantitative.columns[quantitative.isna().sum() > 0]
+
+for col in quant_selected:
+    house_test[col].fillna(house_train[col].mean(), inplace=True)
+house_test[quant_selected].isna().sum()
+
+## 범주형 채우기
+qualitative = house_test.select_dtypes(include = [object])
+qualitative.isna().sum()
+qual_selected = qualitative.columns[qualitative.isna().sum() > 0]
+
+for col in qual_selected:
+    house_test[col].fillna("unknown", inplace=True)
+house_test[qual_selected].isna().sum()
+
+
+house_train.shape
+house_test.shape
+train_n=len(house_train)
+
+# 통합 df 만들기 + 더미코딩
+# house_test.select_dtypes(include=[int, float])
+
+df = pd.concat([house_train, house_test], ignore_index=True)
+# df.info()
+df = pd.get_dummies(
+    df,
+    columns= df.select_dtypes(include=[object]).columns,
+    drop_first=True
+    )
+df
+
+# train / test 데이터셋
+train_df=df.iloc[:train_n,]
+test_df=df.iloc[train_n:,]
+
+## 이상치 탐색
+train_df=train_df.query("GrLivArea <= 4500")
+
+## train
+train_x=train_df.drop("SalePrice", axis=1)
+train_y=train_df["SalePrice"]
+
+## test
+test_x=test_df.drop("SalePrice", axis=1)
+
+
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import GridSearchCV
+
+train_x.shape[0]
+btstrap_index1=np.random.choice(np.arange(1458), 1000, replace=True)
+train_x.iloc
+
+
+model = DecisionTreeRegressor(random_state=42)
+param_grid={
+    'max_depth': np.arange(7, 20, 1),
+    'min_samples_split': np.arange(10, 30, 1)
+}
+grid_search=GridSearchCV(
+    estimator=model,
+    param_grid=param_grid,
+    scoring='neg_mean_squared_error',
+    cv=5
+)
+grid_search.fit(train_x, train_y)
