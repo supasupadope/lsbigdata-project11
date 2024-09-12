05dc2df5ecd9910f32043086c9a72739935dcba4
Issac Lee <issactoast@gmail.com>
2024-09-12 17:35:14 +0900

    클래스 기초

--- code/class.py
+++ code/class.py
@@ -1,7 +1,20 @@
-class Calculator:
+class CookieMaker:
     def __init__(self):
         self.result = 0
 
     def add(self, num):
-        self.result += num
+        self.result = self.result + num
         return self.result
+    
+    def reset(self):
+        self.result=0
+        return self.result
+
+cookie=CookieMaker()
+
+cookie.result
+cookie.add(3)
+cookie.result
+
+cookie.reset()
+cookie.result
