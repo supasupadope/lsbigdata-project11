1ce7ab499a35593152147fc48eea0698c6e28304
Issac Lee <issactoast@gmail.com>
2024-09-12 15:18:10 +0900

    부스팅 모델 설명

--- code/gradientboosting.py
+++ code/gradientboosting.py
@@ -8,16 +8,35 @@ X = np.random.rand(100) - 0.5
 y = 3 * X**2 + 0.05*np.random.randn(100)
 X=X.reshape(100,1)
 
+import matplotlib.pyplot as plt
+plt.scatter(x=X, y=y)
+
 tree_model1=DecisionTreeRegressor(max_depth=2)
 tree_model1.fit(X, y)
+y_tree1=tree_model1.predict(X)
+
+import matplotlib.pyplot as plt
+plt.scatter(x=X, y=y)
+plt.scatter(x=X, y=y_tree1)
 
 y2=y-tree_model1.predict(X)
 tree_model2=DecisionTreeRegressor(max_depth=2)
 tree_model2.fit(X, y2)
+y_tree2=tree_model2.predict(X)
 
-y3=y-tree_model1.predict(X)
+# 두번째 y 데이터 (y-1차 트리 예측값)
+plt.scatter(x=X, y=y2)
+plt.scatter(x=X, y=y_tree2)
+
+y3=y2-tree_model2.predict(X)
 tree_model3=DecisionTreeRegressor(max_depth=2)
 tree_model3.fit(X, y3)
+y_tree3=tree_model3.predict(X)
+
+# 세번째 y 데이터 (y2-2차 트리 예측값)
+plt.scatter(x=X, y=y3)
+plt.scatter(x=X, y=y_tree3)
+
 
 # 새로운 데이터
 X_new=np.array([[0.5], [-0.7], [0.2]])
